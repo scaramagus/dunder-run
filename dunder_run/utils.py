eb50e31f7765ce54482e4db7ee593173e3129278
Agustin Scaramuzza <agustinscaramuzza@gmail.com>
2019-08-06 23:36:34 -0300

    Smart path finding for given filename

--- dunder_run/utils.py
+++ dunder_run/utils.py
@@ -8,7 +8,7 @@ from .exceptions import EntrypointNotFoundError
 def get_dunder_run_from_file(filename: str) -> Callable[..., None]:
     """Extracts the __run__ function from the given Python module.
     """
-    path_to_file = Path('.') / filename
+    path_to_file = Path(filename)
     spec = util.spec_from_file_location('__run__', path_to_file)
     module = util.module_from_spec(spec)
     spec.loader.exec_module(module)
